bc4e6ddb16b98ef57eba15c6d3b203b42670a5b3
Robert Laverty <roblav96@gmail.com>
2019-05-11 07:04:45 -0400

    finsfh

--- final.config.js
+++ final.config.js
@@ -1,11 +1,17 @@
 module.exports = {
 	applications: [
 		{
+			'type': 'application',
 			'name': 'futon-media',
-			'cwd': `${process.env.HOME}/live/futon-media`,
-			'run': 'dist/index.js',
+			'cwd': './',
+			// 'cwd': `${process.env.HOME}/live/futon-media`,
+			// 'base-path': `${process.env.HOME}/live/futon-media`,
+			'run': './dist/index.js',
+			'env': process.env,
 			'ready-on': 'instant',
 			'restart-crashing-delay': 3000,
+			'instances': 1,
+			'mode': 'fork',
 		},
 	],
 }
