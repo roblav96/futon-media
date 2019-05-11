bf7517c67efd8c5a9de07f6f8c127936e21c0f2c
Robert Laverty <roblav96@gmail.com>
2019-05-11 05:53:00 -0400

    [GNU/Linux] npm script fixes

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,10 +1,16 @@
-module.exports = {
-	apps: [
-		{
-			name: 'futon-media',
-			cwd: `${process.env.HOME}/projects/futon-media`,
-			script: 'dist/index.js',
-			log: `${process.env.HOME}/.pm2/logs/futon-media.log`,
-		},
-	],
+const app = {
+	name: 'futon-media',
+
+	cwd: `${process.env.HOME}/live/futon-media`,
+	script: 'dist/index.js',
+
+	output: '/dev/null',
+	error: '/dev/null',
+	log: `${process.env.HOME}/.pm2/logs/futon-media.log`,
+
+	automation: false,
+	pmx: false,
+	vizion: false,
 }
+
+module.exports = { apps: [app] }
