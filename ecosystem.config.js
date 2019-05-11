59af25687f32e724ad80a43672277cc55bf8e3ab
Robert Laverty <roblav96@gmail.com>
2019-05-11 06:11:21 -0400

    pm2 config...

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -4,9 +4,12 @@ const app = {
 	cwd: `${process.env.HOME}/live/futon-media`,
 	script: 'dist/index.js',
 
-	output: '/dev/null',
-	error: '/dev/null',
-	log: `${process.env.HOME}/.pm2/logs/futon-media.log`,
+	// output: '/dev/null',
+	// error: '/dev/null',
+	out_file: './wtf-out.log',
+	error_file: './wtf-error.log',
+	log: './futon-media.log',
+	// log: `${process.env.HOME}/.pm2/logs/futon-media.log`,
 
 	automation: false,
 	pmx: false,
