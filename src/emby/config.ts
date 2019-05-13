5570a0f349ab29b8a488db044a4b9b89fb79ffd7
Robert Laverty <roblav96@gmail.com>
2019-05-13 17:14:45 -0400

    updated deps

--- src/emby/config.ts
+++ src/emby/config.ts
@@ -16,39 +16,42 @@ const REQUIRED: (keyof Env)[] = [
 	'EMBY_STRM_PORT',
 ]
 
-export async function setup() {
-	if (process.env.EMBY_DATA) {
-		let skeys: (keyof SystemXml)[] = []
-		skeys = skeys.concat(['HttpServerPortNumber', 'HttpsPortNumber', 'RequireHttps', 'WanDdns'])
-		let xmlfile = path.join(process.env.EMBY_DATA, 'config/system.xml')
-		if (await fs.pathExists(xmlfile)) {
-			let data = await fs.readFile(xmlfile, 'utf-8')
-			let system = {} as SystemXml
-			skeys.forEach(k => (system[k] = (new RegExp(`<${k}>(.*)<\/${k}>`).exec(data) || [])[1]))
-			let { HttpServerPortNumber, HttpsPortNumber, RequireHttps, WanDdns } = system
-			let xmlenv = {
-				EMBY_HOST: WanDdns,
-				EMBY_PORT: RequireHttps == 'true' ? HttpsPortNumber : HttpServerPortNumber,
-				EMBY_PROTO: RequireHttps == 'true' ? 'https:' : 'http:',
-			} as Env
-			_.defaults(process.env, _.pick(xmlenv, _.keys(xmlenv).filter(k => _.size(xmlenv[k]))))
-		}
-	}
+function defaults(env: Partial<Env>) {
+	_.defaults(process.env, _.pick(env, _.keys(env).filter(k => _.size(env[k]))))
+}
 
-	let surl = `${process.env.EMBY_PROTO || 'http:'}//`
-	surl += `${process.env.EMBY_HOST || '127.0.0.1'}`
-	surl += `:${process.env.EMBY_PORT || 8096}`
-	let { LocalAddress, WanAddress } = (await http.client.get(`${surl}/emby/System/Info/Public`, {
+async function withEmbyData() {
+	let skeys: (keyof SystemXml)[] = []
+	skeys = skeys.concat(['HttpServerPortNumber', 'HttpsPortNumber', 'RequireHttps', 'WanDdns'])
+	let xmlfile = path.join(process.env.EMBY_DATA, 'config/system.xml')
+	if (!(await fs.pathExists(xmlfile))) return
+	let data = await fs.readFile(xmlfile, 'utf-8')
+	let system = {} as SystemXml
+	skeys.forEach(k => (system[k] = (new RegExp(`<${k}>(.*)<\/${k}>`).exec(data) || [])[1]))
+	let { HttpServerPortNumber, HttpsPortNumber, RequireHttps, WanDdns } = system
+	defaults({
+		EMBY_HOST: WanDdns,
+		EMBY_PORT: RequireHttps == 'true' ? HttpsPortNumber : HttpServerPortNumber,
+		EMBY_PROTO: RequireHttps == 'true' ? 'https:' : 'http:',
+	})
+	console.log(`withEmbyData ->`, _.pick(process.env, REQUIRED))
+}
+
+async function withEmbySystemInfo() {
+	let url = `${process.env.EMBY_PROTO || 'http:'}//`
+	url += `${process.env.EMBY_HOST || '127.0.0.1'}`
+	url += `:${process.env.EMBY_PORT || 8096}`
+	let { LocalAddress, WanAddress } = (await http.client.get(`${url}/emby/System/Info/Public`, {
 		silent: true,
 	})) as SystemInfo
-	let url = new Url(process.DEVELOPMENT ? LocalAddress : WanAddress)
-	let sysenv = {
-		EMBY_HOST: url.hostname,
-		EMBY_PORT: url.port,
-		EMBY_PROTO: url.protocol,
-	} as Env
-	_.defaults(process.env, _.pick(sysenv, _.keys(sysenv).filter(k => _.size(sysenv[k]))))
-	console.log(`process.env ->`, _.pick(process.env, REQUIRED))
+	let { hostname, port, protocol } = new Url(process.DEVELOPMENT ? LocalAddress : WanAddress)
+	defaults({ EMBY_HOST: hostname, EMBY_PORT: port, EMBY_PROTO: protocol })
+	console.log(`withEmbySystemInfo ->`, _.pick(process.env, REQUIRED))
+}
+
+export async function setup() {
+	if (process.env.EMBY_DATA) await withEmbyData()
+	await withEmbySystemInfo()
 
 	if (!process.env.EMBY_STRM_PORT) {
 		process.env.EMBY_STRM_PORT = (_.parseInt(process.env.EMBY_PORT) + 3) as any
--- src/index.ts
+++ src/index.ts
@@ -6,7 +6,6 @@ import '@/devops/development'
 import * as config from '@/emby/config'
 
 async function start() {
-	await config.setup()
 	// use dynamic imports to avoid circular null references
 	await import('@/mocks/mocks')
 	await import('@/emby/emby')
@@ -14,10 +13,13 @@ async function start() {
 	await import('@/emby/search')
 	await import('@/emby/strm')
 }
-setTimeout(
-	() => start().catch(error => console.error(`start -> %O`, error)),
-	process.DEVELOPMENT ? 1000 : 1 // wait for 'Debugger attached'
-)
+process.nextTick(async () => {
+	await config.setup()
+	setTimeout(
+		async () => start().catch(error => console.error(`start -> %O`, error)),
+		process.DEVELOPMENT ? 1000 : 1 // wait for 'Debugger attached'
+	)
+})
 
 import * as inspector from 'inspector'
 import exithook = require('exit-hook')
