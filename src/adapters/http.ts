0f4927afde66a7eece1f7605aebe65416853dc7b
Robert Laverty <roblav96@gmail.com>
2019-05-17 00:08:32 -0400

    timeouts

--- src/adapters/http.ts
+++ src/adapters/http.ts
@@ -46,7 +46,7 @@ export class HTTPError extends Error {
 }
 
 export class Http {
-	static timeouts = [5000, 10000, 15000]
+	static timeouts = [10000, 15000]
 	static defaults = {
 		method: 'GET',
 		headers: {
