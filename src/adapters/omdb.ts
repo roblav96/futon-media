5e98f03000c605fdffe3e6bf3f2bb5b5951f912e
Robert Laverty <roblav96@gmail.com>
2019-05-23 07:14:53 -0400

    defaults

--- src/adapters/omdb.ts
+++ src/adapters/omdb.ts
@@ -8,3 +8,39 @@ export const client = new Http({
 	baseUrl: 'https://private.omdbapi.com',
 	query: { apikey: process.env.OMDB_KEY, detail: 'full' },
 })
+
+export interface Result {
+	Actors: string
+	Awards: string
+	BoxOffice: string
+	Country: string
+	Director: string
+	DVD: string
+	Episode: string
+	Error: string
+	Genre: string
+	imdbID: string
+	imdbRating: string
+	imdbVotes: string
+	Language: string
+	Metascore: string
+	Plot: string
+	Poster: string
+	Production: string
+	Rated: string
+	Ratings: {
+		Source: string
+		Value: string
+	}[]
+	Released: string
+	Response: string
+	Runtime: string
+	Season: string
+	seriesID: string
+	Title: string
+	totalSeasons: string
+	Type: string
+	Website: string
+	Writer: string
+	Year: string
+}
