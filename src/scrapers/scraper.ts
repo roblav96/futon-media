e45f4d629bc92640d1989dfb8d2e7e006b61bb46
Robert Laverty <roblav96@gmail.com>
2020-01-30 19:29:04 -0500

    disable ExtraTorrentCm

--- src/scrapers/scraper.ts
+++ src/scrapers/scraper.ts
@@ -46,7 +46,7 @@ process.nextTick(async () => {
 		// (await import('@/scrapers/providers/btbot')).BtBot,
 		(await import('@/scrapers/providers/btdb')).Btdb,
 		(await import('@/scrapers/providers/btsow')).Btsow,
-		(await import('@/scrapers/providers/extratorrent-cm')).ExtraTorrentCm,
+		// (await import('@/scrapers/providers/extratorrent-cm')).ExtraTorrentCm,
 		(await import('@/scrapers/providers/eztv')).Eztv,
 		(await import('@/scrapers/providers/limetorrents')).LimeTorrents,
 		(await import('@/scrapers/providers/magnet4you')).Magnet4You,
