11a27be2cb53b3d623265b8fd1e855bc0bd1b58c
Robert Laverty <roblav96@gmail.com>
2020-05-18 09:28:51 -0400

    dont really need http2 quite yet

--- src/adapters/http2.ts
+++ src/adapters/http2.ts
@@ -1,8 +1,9 @@
+import * as _ from 'lodash'
 import * as http from 'http'
 import * as HttpErrors from 'http-errors'
 import * as normalize from 'normalize-url'
 import * as path from 'path'
-import * as popsicle from 'popsicle/dist/node'
+import * as popsicle from '@/shims/popsicle'
 import * as qs from 'query-string'
 import * as R from 'rambda'
 import * as Url from 'url-parse'
@@ -13,4 +14,70 @@ import { Db } from '@/adapters/db'
 const db = new Db(__filename)
 // process.nextTick(() => process.DEVELOPMENT && db.flush())
 
-popsicle.transport
+export interface Http2Config {
+	baseUrl: string
+	body: any
+	cloudflare: string
+	cookies: boolean
+	debug: boolean
+	delay: number
+	form: any
+	headers: popsicle.HeadersObjectInput
+	json: any
+	memoize: boolean | number
+	method: 'GET' | 'POST' | 'PUT' | 'DELETE'
+	profile: boolean
+	qsArrayFormat: qs.StringifyOptions['arrayFormat']
+	query: Record<string, string | number | string[] | number[]>
+	redirect: boolean
+	retries: number[]
+	silent: boolean
+	url: string
+}
+
+export class Http2 {
+
+
+	constructor(public config = {} as Http2Config) {
+		// _.defaults(this.config, Http2.defaults)
+		// _.mapValues(this.config, (v, k) =>
+		// 	_.isPlainObject(v) ? _.defaults(v, Http2.defaults[k] || {}) : v,
+		// )
+	}
+}
+
+export function toFetch(config: Partial<Http2Config>) {
+	let middleware = popsicle.compose<popsicle.Request, popsicle.HttpResponse>([
+		(req, next) => {
+			if (!req.headers.has('User-Agent')) {
+				// req.headers.set('User-Agent', userAgent)
+			}
+			return next()
+		},
+		// popsicle.userAgent(),
+		popsicle.contentEncoding(),
+		popsicle.redirects(popsicle.compose([popsicle.transport({ keepAlive: 10000 })])),
+	])
+	return popsicle.toFetch(middleware, popsicle.Request)
+}
+
+export const fetch = toFetch({})
+export function get(url: string, config = {} as Http2Config) {
+	return fetch(url, { method: 'GET' } as Parameters<typeof popsicle.fetch>[1])
+}
+
+const trakt = toFetch({
+	baseUrl: 'https://api.trakt.tv',
+	cookies: true,
+	headers: {
+		'content-type': 'application/json',
+		'trakt-api-key': process.env.TRAKT_CLIENT_ID,
+		'trakt-api-version': '2',
+	},
+	query: { extended: 'full' },
+	retries: [408, 500, 502, 503, 504],
+})
+
+process.nextTick(async () => {
+	let res = await get('https://api.trakt.tv')
+})
---
+++ src/shims/popsicle.ts
@@ -0,0 +1,4 @@
+export * from 'popsicle/dist/node'
+export * from 'servie/dist/headers'
+export { compose } from 'throwback'
+export { status } from 'popsicle-status'
