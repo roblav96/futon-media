29a2c599eaff54f0501b981e7d6eb7ff3146b26d
Robert Laverty <roblav96@gmail.com>
2020-12-15 17:27:14 -0500

    [Darwin] M  jellyfin/nghttpx.conf

--- src/debrids/debrids.ts
+++ src/debrids/debrids.ts
@@ -12,7 +12,7 @@ import * as utils from '@/utils/utils'
 import pQueue from 'p-queue'
 import { Premiumize } from '@/debrids/premiumize'
 import { RealDebrid } from '@/debrids/realdebrid'
-import { AllDebrid } from '@/debrids/alldebrid'
+// import { AllDebrid } from '@/debrids/alldebrid'
 import { Torrent } from '@/scrapers/torrent'
 
 export const debrids = {
