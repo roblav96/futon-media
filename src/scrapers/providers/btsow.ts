642dfacfb0cdc6553313dd8176b37672e7bab14e
Robert Laverty <roblav96@gmail.com>
2022-01-17 12:38:06 -0500

    Update btsow.ts

--- src/scrapers/providers/btsow.ts
+++ src/scrapers/providers/btsow.ts
@@ -2,14 +2,12 @@ import * as _ from 'lodash'
 import * as cheerio from 'cheerio'
 import * as dayjs from 'dayjs'
 import * as http from '@/adapters/http'
-import * as path from 'path'
-import * as qs from '@/shims/query-string'
 import * as scraper from '@/scrapers/scraper'
 import * as uastring from 'ua-string'
 import * as utils from '@/utils/utils'
 
 export const client = scraper.Scraper.http({
-	baseUrl: 'https://btos.pw',
+	baseUrl: 'https://btsow.rest',
 	headers: { 'user-agent': uastring },
 })
 
@@ -17,18 +15,17 @@ export class Btsow extends scraper.Scraper {
 	async getResults(slug: string) {
 		let $ = cheerio.load(await client.get(`/search/${slug}`))
 		let results = [] as scraper.Result[]
-		$('.data-list > .row:has(a)').each((i, el) => {
+		$('div.row:has(a[href*="/detail/hash/"])').each((i, el) => {
 			try {
 				let $el = $(el)
-				let link = $el.find('a')
-				let href = link.attr('href')
+				let link = $el.find('a[href*="/detail/hash/"]')
 				let title = link.attr('title') || link.text()
 				results.push({
-					bytes: utils.toBytes($el.find('.size').text()),
+					bytes: utils.toBytes($el.find('div.size').text()),
 					name: title,
-					magnet: `magnet:?xt=urn:btih:${href.split('/').pop()}&dn=${title}`,
+					magnet: `magnet:?xt=urn:btih:${link.attr('href').split('/').pop()}&dn=${title}`,
 					seeders: 1,
-					stamp: utils.toStamp($el.find('.date').text()),
+					stamp: dayjs($el.find('div.date').text()).valueOf(),
 				} as scraper.Result)
 			} catch (error) {
 				console.error(`${this.constructor.name} -> %O`, error.message)
@@ -36,8 +33,4 @@ export class Btsow extends scraper.Scraper {
 		})
 		return results
 	}
-}
-
-interface Query {
-	q: string
 }
